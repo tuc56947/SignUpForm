03f8cf8cd58d0b21afdbaa8bb8ad2d7a7e2e34b9
David Lee <tuc56947@temple.edu>
2021-02-16 17:40:31 -0500

    changed textView to editText and added onclick function on button where it checks if all fields have been filled

--- app/src/main/java/edu/temple/signupform/FormActivity.java
+++ app/src/main/java/edu/temple/signupform/FormActivity.java
@@ -3,6 +3,10 @@ package edu.temple.signupform;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class FormActivity extends AppCompatActivity {
 
@@ -10,5 +14,25 @@ public class FormActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button saveButton = (Button) findViewById(R.id.save);
+
+        saveButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                EditText name = (EditText)(findViewById(R.id.userInput_Name));
+                EditText email = (EditText)(findViewById(R.id.userInput_Email));
+                EditText password = (EditText)(findViewById(R.id.userInput_Password));
+                EditText passwordConfirm = (EditText)(findViewById(R.id.userInput_PasswordConfirm));
+                if(isEmpty(name) || isEmpty(email) || isEmpty(password) || isEmpty(passwordConfirm)) {
+                    Toast.makeText(FormActivity.this, "Fill in all fields", Toast.LENGTH_LONG).show();
+                }
+            }
+
+        });
+    }
+
+    private boolean isEmpty(EditText edText) {
+        return edText.getText().toString().trim().length() == 0;
     }
 }
